0da57774ab5a88c13d8e66e831d0d22d5ec17127
xGiye <amadionengiyewilliams@gmail.com>
2023-09-17 12:05:30 +0100

    using put instead of patch

--- Routes/persons-route.js
+++ Routes/persons-route.js
@@ -7,7 +7,7 @@ router.get("/:name", personController.getPersonByName);
 router.get("/", personController.getPersons);
 router.post("/", personController.createPerson);
 
-router.patch("/:name", personController.updatePerson);
+router.put("/:name", personController.updatePerson);
 router.delete("/:name", personController.deletePerson);
 
 module.exports = router;
